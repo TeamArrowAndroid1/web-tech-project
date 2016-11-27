eaa9d595476bb3409f7eb2a63c59958cba8bcb79
teamarrowandroid <hssabharwal00@gmail.com>
2016-11-27 13:51:55 -0500

    new changes and resolving data connection with server

---
+++ Downloads/MyApplication11/app/src/main/java/test/myapplication/Patient_DATA.java
@@ -0,0 +1,18 @@
+package test.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Patient_DATA extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient__dat);
+
+
+
+    }
+}
